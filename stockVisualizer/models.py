1ceb4ced6a44be4821ad52af40671b37aa526636
Kevin Downey <kevindowney@Kevins-MacBook-Pro.local>
2021-10-26 19:23:33 -0400

    Initial commit

---
+++ stockVisualizer/models.py
@@ -0,0 +1,9 @@
+
+# models.py
+
+from django.db import models
+
+# Create your models here.
+class StockData(models.Model):
+    symbol = models.TextField(null=True)
+    data = models.TextField(null=True)
